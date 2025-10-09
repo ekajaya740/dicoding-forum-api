01211301613fa96d2c51440b31e773351c35c588
ekajaya740 <ekajaya740@gmail.com>
2025-10-09 14:03:10 +0800

    feat: add thread table

---
+++ migrations/1759988743721_create-table-threads.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('threads', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('threads');
+};
